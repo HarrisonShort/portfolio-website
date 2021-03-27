9b00a265c4878e7f4bb58ae441cd9d8e10b5a112
HarrisonShort <work.harrisonshort@gmail.com>
2021-03-27 15:13:45 +1000

    REMOVE footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,18 +5,6 @@ import './Footer.css'
 export default function Footer() {
     return (
         <div className="footer-container">
-            <div className="footer-links">
-                <div className="footer-links-wrapper">
-                    <div className="footer-links-items">
-                        <h2>Navigation</h2>
-                        <Link to="/">Home</Link>
-                        <Link to="/Portfolio">Portfolio</Link>
-                        <Link to="/Blog">Blog</Link>
-                        <Link to="/About">About</Link>
-                        <Link to="/Contact">Contact</Link>
-                    </div>
-                </div>
-            </div>
             <small className="footer-copyright">
                 Harrison Short (c) 2021
              </small>
