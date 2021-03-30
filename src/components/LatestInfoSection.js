38ab26e6d9afe41773491519c3632edd17e4d1fb
HarrisonShort <work.harrisonshort@gmail.com>
2021-03-30 21:03:37 +1000

    ADD BlogPostPreview and LatestInfoSection HTML and CSS

--- src/components/LatestInfoSection.js
+++ src/components/LatestInfoSection.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import BlogPostPreview from './BlogPostPreview';
+import './LatestInfoSection.css'
 
 function LatestInfoSection() {
     return (
-        <div>
-            <div className="current-project">
+        <div className="latest-info">
+            <div className="latest-project">
                 <h1>Latest Project</h1>
-                {/* image here */
+                <div className="latest-project-info">
+                    <div className="latest-project-image">
+                        <img src="/images/harrison_okunoshima.jpg" alt="Harrison Okunoshima" className="info-image" />
+                    </div>
+                    <div className="latest-project-text">
+                        <p className="latest-project-title">This website!</p>
+                        <p className="latest-project-blurb">I am currently working on this website, which will eventually be my portfolio!</p>
+                    </div>
 
-                /* short blurb about project, from the project data */}
-
+                </div>
             </div>
             <div className="blog-posts__container">
                 <h1>Recent Blog Posts</h1>
@@ -18,12 +25,12 @@ function LatestInfoSection() {
                         <BlogPostPreview
                             title="Test Blog Post 1"
                             date="21st March 2021"
-                            text="this is preview text 1"
+                            text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
                             path="/blog" />
                         <BlogPostPreview
                             title="Test Blog Post 2"
                             date="21st March 2021"
-                            text="this is preview text 2"
+                            text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
                             path="/blog" />
                         <BlogPostPreview
                             title="Test Blog Post 3"
@@ -32,9 +39,8 @@ function LatestInfoSection() {
                             path="/blog" />
                     </ul>
                 </div>
-
             </div>
-        </div>
+        </div >
     )
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,6 +7,7 @@ export default function Home() {
     return (
         <React.Fragment>
             <PersonalInfoSection />
+            <hr />
             <LatestInfoSection />
         </React.Fragment>
     )
