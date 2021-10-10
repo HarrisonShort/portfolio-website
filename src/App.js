b0d949bf67b628064cd32a3331a57b3ff64433d8
HarrisonShort <work.harrisonshort@gmail.com>
2021-10-10 13:23:38 +1000

    CHANGE location of files

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './components/pages/Home';
+import Home from './components/pages/home/Home';
 import Portfolio from './components/pages/portfolio/Portfolio';
-import Blog from './components/pages/Blog';
-import About from './components/pages/About';
-import Contact from './components/pages/Contact';
+//import Blog from './components/pages/blog/Blog';
+import About from './components/pages/about/About';
+import Contact from './components/pages/contact/Contact';
 import Footer from './components/Footer';
 
 class App extends Component {
@@ -18,7 +18,7 @@ class App extends Component {
                     <Switch>
                         <Route path='/' exact component={Home} />
                         <Route path='/portfolio' exact component={Portfolio} />
-                        <Route path='/blog' exact component={Blog} />
+                        {/* <Route path='/blog' exact component={Blog} /> */}
                         <Route path='/about' exact component={About} />
                         <Route path='/contact' exact component={Contact} />
                     </Switch>
---
+++ src/components/pages/contact/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../../App.css'
+
+export default function Contact() {
+    return (
+        <React.Fragment>
+            <h1 className="contact">CONTACT</h1>
+        </React.Fragment>
+
+    )
+}
