bb85cda4d48378562dc5b5ba6bca8ab6971465d0
HarrisonShort <work.harrisonshort@gmail.com>
2021-10-13 18:06:08 +1000

    FIX contact form fuckery

--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -1,61 +1,100 @@
+import axios from 'axios';
 import React from 'react'
 import '../../../App.css'
 import './Contact.css'
 
-export default function Contact() {
+const formId = 'RDu8E3Mo';
+const formSparkUrl = `https://submit-form.com/${formId}`;
 
-    const submitForm = () => { };
+async function postSubmission() {
+    const payload = {
+        message: 'Test formspark submission',
+    };
 
-    return (
-        <React.Fragment>
-            <h1 id="page-header">CONTACT</h1>
-            <div div className="page-container">
+    try {
+        //const result = await axios.post(formSparkUrl, payload);
+        //console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+async function submitForm() {
+    event.preventDefault();
+    await postSubmission();
+};
+
+export default class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sender: '',
+            email: '',
+            message: ''
+        };
+
+        console.log(this.state.sender);
+        console.log(this.state.email);
+        console.log(this.state.message);
+
+        this.handleChange = this.handleChange.bind(this);
+    };
+
+    handleChange(event) {
+
+
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+
+        console.log(this.state);
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1 id="page-header">CONTACT</h1>
                 <div className="form-container">
                     <form onSubmit={submitForm}>
                         <div className="form-element">
-                            <label htmlFor="name-input">Name</label>
-                            <input type="text" id="name-input"></input>
+                            <label htmlFor="sender-input">Name</label>
+                            <input type="text" name="sender" value={this.state.sender} onChange={this.handleChange} />
                         </div>
 
                         <div className="form-element">
                             <label htmlFor="email-input" className="formLabel">Email</label>
-                            <input type="text" id="email-input"></input>
+                            <input type="text" name="email" value={this.state.email} onChange={this.handleChange} />
                         </div>
 
                         <div className="form-element">
-                            <label for="message-input">Message</label>
-                            <textarea id="message-input"></textarea>
+                            <label htmlFor="message-input">Message</label>
+                            <textarea name="message" value={this.state.message} onChange={this.handleChange} />
                         </div>
 
                         <button className="submit-button">Send Message!</button>
                     </form>
-                </div>
-
-                {/* <div className="info-subtext">
-                    <p>You can connect with me here!</p>
-                    <br></br>
-                    <ul>
+                </div >
+                <div className="info-subtext">
+                    <p>You can also message me here!</p>
+                    <ul className="social-ul">
                         <li>
                             <a href="https://www.linkedin.com/in/harrison-short/" className="href" target="_blank">
-                                <i class="fab fa-linkedin"></i>LinkedIn
+                                <i className="fab fa-linkedin"></i>LinkedIn
                             </a>
                         </li>
                         <li>
                             <a href="https://github.com/HarrisonShort" className="href" target="_blank">
-                                <i class="fab fa-github"></i>GitHub
+                                <i className="fab fa-github"></i>GitHub
                             </a>
                         </li>
                         <li>
                             <a href="https://twitter.com/TheHarrishun" className="href" target="_blank">
-                                <i class="fab fa-twitter"></i>Twitter
+                                <i className="fab fa-twitter"></i>Twitter
                             </a>
                         </li>
                     </ul>
-
-                </div> */}
-            </div>
-
-        </React.Fragment>
-
-    )
+                </div>
+            </React.Fragment >
+        )
+    }
 }
