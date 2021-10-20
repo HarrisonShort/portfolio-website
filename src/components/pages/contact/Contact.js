4ed19a116e115a6432c122e41834e09a4ca6d162
HarrisonShort <work.harrisonshort@gmail.com>
2021-10-20 12:16:54 +1000

    CHANGE Contact from being a Class Component to a Functional Component

--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -1,105 +1,122 @@
 import axios from 'axios';
-import React from 'react'
+import React, { useState } from 'react'
 import '../../../App.css'
 import './Contact.css'
 import ReCAPTCHA from 'react-google-recaptcha';
 
-const formId = 'RDu8E3Mo';
-const formSparkUrl = `https://submit-form.com/${formId}`;
-const recaptchaKey = '6Ldj198cAAAAAJqHKPvEmfMO5bH2w1sVWOAApkI-';
+export default function Contact() {
+    const formId = 'RDu8E3Mo';
+    const formSparkUrl = `https://submit-form.com/${formId}`;
+    const recaptchaKey = '6Ldj198cAAAAAJqHKPvEmfMO5bH2w1sVWOAApkI-';
+    let recaptchaToken = '';
 
-async function postSubmission() {
-    const payload = {
-        ...this.state
+    const [sender, setSender] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [responseMessage, setResponseMessage] = useState('');
+
+    const handleSenderInput = (event) => {
+        setSender(event.target.value);
     };
 
-    try {
-        //const result = await axios.post(formSparkUrl, payload);
-        //console.log(result);
-    } catch (error) {
-        console.log(error);
+    const handleEmailInput = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handleMessageInput = (event) => {
+        setMessage(event.target.value);
+    };
+
+    const handleResponseMessage = (message) => {
+        setResponseMessage(message);
+    };
+
+    const updateRecaptchaToken = (token) => {
+        recaptchaToken = token;
     }
-};
 
-async function submitForm() {
-    event.preventDefault();
-    await postSubmission();
-};
+    const resetFields = () => {
+        setSender('');
+        setEmail('');
+        setMessage('');
+    }
 
-function updateRecaptchaToken() {
-
-}
-
-export default class Contact extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            sender: '',
-            email: '',
-            message: ''
+    const postSubmission = async () => {
+        const payload = {
+            sender: sender,
+            email: email,
+            message: message,
+            "g-recaptcha-response": recaptchaToken
         };
 
-        this.handleChange = this.handleChange.bind(this);
+        window.grecaptcha.reset();
+
+        try {
+            const result = await axios.post(formSparkUrl, payload);
+            console.log(result);
+            setResponseMessage('test')
+            resetFields();
+        } catch (error) {
+            setResponseMessage('error')
+        }
     };
 
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    };
-
-    render() {
-        return (
-            <React.Fragment>
-                <h1 id="page-header">CONTACT</h1>
-                <div className="form-container">
-                    <form onSubmit={submitForm}>
-                        <div className="form-element">
-                            <label htmlFor="sender-input">Name</label>
-                            <input type="text" name="sender" value={this.state.sender} onChange={this.handleChange} />
-                        </div>
-
-                        <div className="form-element">
-                            <label htmlFor="email-input" className="formLabel">Email</label>
-                            <input type="text" name="email" value={this.state.email} onChange={this.handleChange} />
-                        </div>
-
-                        <div className="form-element">
-                            <label htmlFor="message-input">Message</label>
-                            <textarea name="message" value={this.state.message} onChange={this.handleChange} />
-                        </div>
-
-                        <div className="form-element">
-                            <ReCAPTCHA
-                                sitekey={recaptchaKey}
-                                onChange={updateRecaptchaToken}
-                            />
-                        </div>
-
-                        <button className="submit-button">Send Message!</button>
-                    </form>
-                </div >
-                <div className="info-subtext">
-                    <p>You can also message me here!</p>
-                    <ul className="social-ul">
-                        <li>
-                            <a href="https://www.linkedin.com/in/harrison-short/" className="href" target="_blank">
-                                <i className="fab fa-linkedin"></i>LinkedIn
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/HarrisonShort" className="href" target="_blank">
-                                <i className="fab fa-github"></i>GitHub
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://twitter.com/TheHarrishun" className="href" target="_blank">
-                                <i className="fab fa-twitter"></i>Twitter
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </React.Fragment >
-        )
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        await postSubmission();
     }
+
+    return (
+        <React.Fragment>
+            <h1 id="page-header">CONTACT</h1>
+            <div className="form-container">
+                <form onSubmit={handleSubmit}>
+                    <div className="form-element">
+                        <label htmlFor="sender-input">Name</label>
+                        <input type="text" name="sender" value={sender} onChange={handleSenderInput} />
+                    </div>
+
+                    <div className="form-element">
+                        <label htmlFor="email-input" className="formLabel">Email</label>
+                        <input type="text" name="email" value={email} onChange={handleEmailInput} />
+                    </div>
+
+                    <div className="form-element">
+                        <label htmlFor="message-input">Message</label>
+                        <textarea name="message" value={message} onChange={handleMessageInput} />
+                    </div>
+
+                    <div className="form-element">
+                        <ReCAPTCHA
+                            sitekey={recaptchaKey}
+                            onChange={updateRecaptchaToken}
+                        />
+                    </div>
+
+                    <button className="submit-button">Send Message!</button>
+                    <div id="response-message">{responseMessage}</div>
+                </form>
+            </div >
+            <div className="info-subtext">
+                <p>You can also message me here!</p>
+                <ul className="social-ul">
+                    <li>
+                        <a href="https://www.linkedin.com/in/harrison-short/" className="href" target="_blank">
+                            <i className="fab fa-linkedin"></i>LinkedIn
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/HarrisonShort" className="href" target="_blank">
+                            <i className="fab fa-github"></i>GitHub
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/TheHarrishun" className="href" target="_blank">
+                            <i className="fab fa-twitter"></i>Twitter
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </React.Fragment >
+    )
 }
