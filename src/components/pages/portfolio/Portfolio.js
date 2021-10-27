d7298cbb4ac5945b5356f9b7d2a7245582c4d915
HarrisonShort <work.harrisonshort@gmail.com>
2021-10-27 15:14:46 +1000

    ADD most of the styling

--- src/components/pages/portfolio/Portfolio.js
+++ src/components/pages/portfolio/Portfolio.js
@@ -25,7 +25,7 @@ export default function Portfolio() {
             <div className="grid-container">
                 <PortfolioThumbnail
                     imgSrc="/images/harrison_okunoshima.jpg"
-                    title="test"
+                    title="Portfolio Website"
                     onClick={() => onThumbnailClicked({
                         title: "test",
                         year: 2021,
@@ -36,15 +36,21 @@ export default function Portfolio() {
                     })}
                 />
                 <PortfolioThumbnail
-                    imgSrc="/images/harrison_okunoshima.jpg"
-                    title="test"
+                    imgSrc="\images\portfolio\bees-wont-exist\Logo+Transparent.png"
+                    title="Bees Won't Exist"
                     onClick={() => onThumbnailClicked({
-                        title: "test",
-                        year: 2021,
-                        details: "React, Javascript, etc.",
-                        blurb: "This is my portfolio website.",
-                        tags: "React",
-                        screenshots: "test",
+                        title: "Bees Won't Exist",
+                        //#region Blurb
+                        blurb: "GIANT BEES HAVE DECIMATED HUMANITY! Every day, the low buzz of death approaches further. Can you hear it? Don’t worry! It’s time for some pest control. Grab your sword. I’ll meet you by the hive! Whatever you do though, do not touch those glowing purple orbs. Don't worry about why. OK?\nBees Won't Exist is an isometric-perspective hack-and-slash game, where the player plays as a young boy named Kika. Kika is part of the human rebellion, fighting back against the Giant Bees that have taken over the Earth and, in some parts, have enslaved humanity. Players utilize dash attacks, strong attacks and dodges, whilst navigating various honey-based puzzles throughout their 1 to 3+ hour gameplay experience.\n---\nBees Won't Exist was the project I worked on during my final year in QUT's Bachelor of Games and Interactive Entertainment.Every student completing this course is initially tasked with prototyping a game idea, and then from there, the prototype is developed upon, extensive playtesting and internal QA is done and the game moves quickly into the marketing/publishing phase. For such a short time span (approximately 8 months from the start of prototype to publishing), Bees Won't Exist was extremely ambitious, but after many sleepless nights and moments of panic and despair, I'm truly proud of my contributions to the project. I served as the Sound Lead on the game, as well as a Programmer.\nIn addition, Bees Won't Exist won a place at Game Connect Asia Pacific (GCAP) 2016's student showcase down in Melbourne, Australia. I was extremely proud and honoured to have received the opportunity so early in my career!",
+                        //#endregion
+                        studio: "Honeyvale Games",
+                        platform: "PC/Mac/Linux",
+                        releaseDate: "October 25th 2016",
+                        progress: "Complete!",
+                        role: "Sound Lead, Programmer",
+                        engine: "Unity (C#)",
+                        screenshots: "/images/portfolio/bees-wont-exist/Logo+Transparent.png",
+                        links: "https://gamejolt.com/games/bees-won-t-exist/194883",
                         closeOverlay: onOverlayClosed
                     })}
                 />
--- src/components/pages/portfolio/PortfolioOverlay.js
+++ src/components/pages/portfolio/PortfolioOverlay.js
@@ -2,25 +2,40 @@ import React, { useState } from 'react'
 import './PortfolioOverlay.css'
 
 export default function PortfolioOverlay(props) {
+    let splitBlurb = props.data.blurb.split('\n').map(i => {
+        return <p className="blurb-split">{i}</p>
+    });
+
     return (
         <React.Fragment>
-            <div className="portfolio-overlay" onClick={props.data.closeOverlay}>
-                <div className="project-information" onClick>
-                    <div className="project-container">
-                        <button className="close-button" onClick={props.data.closeOverlay}>X</button>
-                        <div>
+            <div className="overlay-background" onClick={props.data.closeOverlay}>
+                <div className="project-container" onClick>
+                    <div className="scroll-area-container">
+                        <div className="project-info">
+                            <button className="close-button" onClick={props.data.closeOverlay}>X</button>
                             <p className="title">{props.data.title}</p>
-                            <p className="year">{props.data.year}</p>
+                            <div className="left-side">
+                                {props.data.blurb ? <div className="blurb">{splitBlurb}</div> : null}
+                                {props.data.tags ? <div className="tags">{props.data.tags}</div> : null}
+                            </div>
+                            <div className="right-side">
+                                {/* //TODO: Change to be a slideshow/open full image in new tab */}
+                                {props.data.screenshots ? <img className="screenshots" src={props.data.screenshots} alt=""></img> : null}
+                                {props.data.studio ? <p><b>Studio: </b>{props.data.studio}</p> : null}
+                                {props.data.plaform ? <p><b>Platform: </b>{props.data.plaform}</p> : null}
+                                {props.data.releaseDate ? <p><b>Release Date: </b>{props.data.releaseDate}</p> : null}
+                                {props.data.progress ? <p><b>Progress: </b>{props.data.progress}</p> : null}
+                                {props.data.role ? <p><b>My Role: </b>{props.data.role}</p> : null}
+                                {props.data.engine ? <p><b>Engine: </b>{props.data.engine}</p> : null}
+                                {props.data.links ? <p><b>Links: </b><a href={props.data.links}>Download</a></p> : null}
+
+                            </div>
                         </div>
 
-                        <p className="details">{props.data.details}</p>
-                        <p className="blurb">{props.data.blurb}</p>
-                        {props.data.screenshots ? <div className="screenshots">{props.data.screenshots}</div> : null}
-                        <div className="tags">{props.data.tags}</div>
                     </div>
 
                 </div>
             </div>
-        </React.Fragment>
+        </React.Fragment >
     )
 }
