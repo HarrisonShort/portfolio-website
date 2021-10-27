521df5e7d5a9f074a8fcdebacb916e7ce5ae2b50
HarrisonShort <work.harrisonshort@gmail.com>
2021-10-27 10:39:29 +1000

    ADD PortfolioOverlay files

---
+++ src/components/pages/portfolio/PortfolioOverlay.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import './PortfolioOverlay.css'
+
+export default function PortfolioOverlay(props) {
+    return (
+        <React.Fragment>
+            <div className="portfolio-overlay"></div>
+        </React.Fragment>
+    )
+}
