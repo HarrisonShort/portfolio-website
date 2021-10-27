2ac532621ce6cb5e61731a4d813c6efebb7b74a6
HarrisonShort <work.harrisonshort@gmail.com>
2021-10-27 10:39:42 +1000

    ADD basic functionality (I think this is how it should be done???)

--- src/components/pages/portfolio/Portfolio.js
+++ src/components/pages/portfolio/Portfolio.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../../App.css'
 import './Portfolio.css'
 import PortfolioThumbnail from './PortfolioThumbnail'
 
 export default function Portfolio() {
+    const [overlay, setOverlay] = useState(null);
+
+    const overlayShowing = (overlay != null);
+
+    const onThumbnailClicked = (overlayInfo) => {
+        setOverlay(overlayInfo);
+    }
+
     return (
         <React.Fragment>
             <h1 id="page-header">PORTFOLIO</h1>
+            <div className="overlay">
+                {overlayShowing ? overlay : null}
+            </div>
             <div className="grid-container">
-                <PortfolioThumbnail imgSrc="/images/harrison_okunoshima.jpg" title="test" />
+                <PortfolioThumbnail imgSrc="/images/harrison_okunoshima.jpg" title="test" onClick={onThumbnailClicked} />
                 <PortfolioThumbnail imgSrc="/images/harrison_okunoshima.jpg" title="test" />
                 <PortfolioThumbnail imgSrc="/images/harrison_okunoshima.jpg" title="test" />
                 <PortfolioThumbnail imgSrc="/images/harrison_okunoshima.jpg" title="test" />
--- src/components/pages/portfolio/PortfolioThumbnail.js
+++ src/components/pages/portfolio/PortfolioThumbnail.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import PortfolioOverlay from './PortfolioOverlay'
 import './PortfolioThumbnail.css'
 
 export default function PortfolioThumbnail(props) {
+    const onThumbnailClicked = () => {
+        props.onClick(
+            <PortfolioOverlay></PortfolioOverlay>
+        );
+    }
+
     return (
-        <div className="imageContainer">
+        <div className="imageContainer" onClick={onThumbnailClicked}>
             <img src={props.imgSrc} alt="Harrison Okunoshima" className="thumbnailImage" />
             <p className="title">{props.title}</p>
         </div>
