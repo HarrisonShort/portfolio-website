833531d5d8ffeb663ee0fcae36f046430a22f1e4
HarrisonShort <work.harrisonshort@gmail.com>
2021-10-27 15:14:23 +1000

    FIX sizing issue with PortfolioThumbnail

--- src/components/pages/portfolio/PortfolioThumbnail.js
+++ src/components/pages/portfolio/PortfolioThumbnail.js
@@ -3,13 +3,6 @@ import PortfolioOverlay from './PortfolioOverlay'
 import './PortfolioThumbnail.css'
 
 export default function PortfolioThumbnail(props) {
-    const onThumbnailClicked = () => {
-        // console.log(props.data.imgSrc);
-        // props.data.onClick(
-        //     <PortfolioOverlay></PortfolioOverlay>
-        // );
-    }
-
     return (
         <div className="imageContainer" onClick={props.onClick}>
             <img src={props.imgSrc} alt="Harrison Okunoshima" className="thumbnailImage" />
